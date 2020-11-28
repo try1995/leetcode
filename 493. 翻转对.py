f0b8d032cb8368da2c131c7527888b0ce73235a4
try <1635341612@qq.com>
2020-11-28 21:35:12 +0800

    leetcode

--- 493. 翻转对.py
+++ 493. 翻转对.py
@@ -1,6 +1,21 @@
+from collections import Counter
+
+
 class Solution(object):
     def reversePairs(self, nums):
         """
         :type nums: List[int]
         :rtype: int
         """
+        count = Counter(nums)
+        ret = 0
+        for i in nums[::-1]:
+            count[i] -= 1
+            if count[i] == 0:
+                count.pop(i)
+            ret += sum([count[j] for j in count if i > 2*j])
+        return ret
+
+s = Solution()
+data = s.reversePairs([2,4,3,5,1])
+print(data)
