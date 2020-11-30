77eefad2359018251b9032536e7428d4eeacb36e
try <1635341612@qq.com>
2020-12-01 00:28:20 +0800

    leetcode

--- 767. 重构字符串.py
+++ 767. 重构字符串.py
@@ -11,17 +11,18 @@ class Solution(object):
         count = sorted(count.items(), key=lambda x: x[1])
         if len(count) < 2:
             return ""
-        flag = count[0][1]
-        ret = [count[0][0]] * count[0][1]
-        count.pop(0)
-        for value, nums in count:
-            if nums <= flag + 1:
-                flag = flag + nums + 1
-                for i in range(nums):
-                    ret.insert(2*i, value)
-            else:
-                return ""
-        return "".join(ret)
+        queue = []
+        while count:
+            queue.append(count[0][0])
+            queue.append(count[1][0])
+            for i in range(len(count)-1):
+                if count[i][1] < count[i+1][1]:
+                    temp = count[i]
+                    count[i] = count[i+1]
+                    count[i+1] = temp
+                else:
+                    break
+        return "".join(queue)
 
 
 s = Solution()
