f84380b9606fd3c0d6971c7f1dde8f939809097e
try <1635341612@qq.com>
2020-12-01 23:32:26 +0800

    应该用二分查找，偷懒了

--- 34. 在排序数组中查找元素的第一个和最后一个位置.py
+++ 34. 在排序数组中查找元素的第一个和最后一个位置.py
@@ -1,7 +1,27 @@
 class Solution(object):
-    def searchRange(self, nums, target):
+    def searchRange(self, nums:list, target):
         """
         :type nums: List[int]
         :type target: int
         :rtype: List[int]
         """
+        if not nums:
+            return [-1, -1]
+        try:
+            left = nums.index(target)
+            right = left
+            while True:
+                if right+1 == len(nums):
+                    break
+                if nums[right+1] == target:
+                    right += 1
+                else:
+                    break
+            return [left, right]
+        except:
+            return [-1, -1]
+
+
+s = Solution()
+data = s.searchRange(nums = [1], target = 1)
+print(data)
