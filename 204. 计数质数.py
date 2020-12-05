51ee847fd3fc4fad56a1bd8b9f2e372dd4afcfda
try <1635341612@qq.com>
2020-12-05 23:30:55 +0800

    leetcode

--- 204. 计数质数.py
+++ 204. 计数质数.py
@@ -1,2 +1,20 @@
 class Solution:
+    def check(self, num):
+        for i in range(2,int(pow(num,0.5))+1):
+            if num % i == 0:
+                return False
+        return True
+
     def countPrimes(self, n: int) -> int:
+        if n < 2:
+            return 0
+        count = 0
+        for i in range(2, n):
+            if self.check(i):
+                count += 1
+        return count
+
+
+s = Solution()
+data = s.countPrimes(10)
+print(data)
