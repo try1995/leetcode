82ebac7c20b979bcd5844ce2d846cb70c5bf60fe
try <1635341612@qq.com>
2020-12-18 21:21:02 +0800

    leetcode-动态规划

--- 714. 买卖股票的最佳时机含手续费.py
+++ 714. 买卖股票的最佳时机含手续费.py
@@ -5,3 +5,14 @@ class Solution(object):
         :type fee: int
         :rtype: int
         """
+        n = len(prices)
+        dp = [[0, -prices[0]]] + [[0, 0] for _ in range(n - 1)]
+        for i in range(1, n):
+            dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] + prices[i] - fee)
+            dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] - prices[i])
+        return dp[n - 1][0]
+
+
+s = Solution()
+data = s.maxProfit(prices=[1, 3, 2, 8, 4, 9], fee=2)
+print(data)
