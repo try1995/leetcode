05d9bab18eedc8a8f7c47e651ef25ae0545b43d3
try <1635341612@qq.com>
2021-12-31 16:12:53 +0800

    leetcode

---
+++ 算法与数据结构/二分查找.py
@@ -0,0 +1,12 @@
+class Solution:
+    def search(self, nums: list, target: int) -> int:
+        beg, end = 0, len(nums) - 1
+        while beg <= end:
+            mid = (beg + end) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                end = mid - 1
+            else:
+                beg = mid + 1
+        return -1
---
+++ 算法与数据结构/前缀树.py
@@ -0,0 +1,60 @@
+"""
+Trie（发音类似 "try"）或者说 前缀树 是一种树形数据结构，用于高效地存储和检索字符串数据集中的键。
+这一数据结构有相当多的应用情景，例如自动补完和拼写检查。
+
+请你实现 Trie 类：
+
+Trie() 初始化前缀树对象。
+void insert(String word) 向前缀树中插入字符串 word 。
+boolean search(String word) 如果字符串 word 在前缀树中，返回 true（即，在检索之前已经插入）；否则，返回 false 。
+boolean startsWith(String prefix) 如果之前已经插入的字符串 word 的前缀之一为 prefix ，返回 true ；否则，返回 false 。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/implement-trie-prefix-tree
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+
+
+class Trie:
+    def __init__(self):
+        self.children = [None] * 26
+        self.isEnd = False
+
+    def searchPrefix(self, prefix: str) -> "Trie":
+        node = self
+        for ch in prefix:
+            ch = ord(ch) - ord("a")
+            if not node.children[ch]:
+                return None
+            node = node.children[ch]
+        return node
+
+    def insert(self, word: str) -> None:
+        node = self
+        for ch in word:
+            ch = ord(ch) - ord("a")
+            if not node.children[ch]:
+                node.children[ch] = Trie()
+            node = node.children[ch]
+        node.isEnd = True
+
+    def search(self, word: str) -> bool:
+        node = self.searchPrefix(word)
+        return node is not None and node.isEnd
+
+    def startsWith(self, prefix: str) -> bool:
+        return self.searchPrefix(prefix) is not None
+
+    # 给你一个不含重复单词的字符串数组 words，请你找出并返回words中的所有 连接词。
+    # 连接词定义为：一个完全由给定数组中的至少两个较短单词组成的字符串。
+    def dfs(self, word: str, start: int) -> bool:
+        if start == len(word):
+            return True
+        node = self
+        for i in range(start, len(word)):
+            node = node.children[ord(word[i]) - ord('a')]
+            if node is None:
+                return False
+            if node.isEnd and self.dfs(word, i + 1):
+                return True
+        return False
